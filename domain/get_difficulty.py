4bd7daf06870b7d4f118cd8ee4a77b2fd5425ae3
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-10-30 10:42:54 -0600

    chore: factorized the get_difficulty function from main to an independent file

---
+++ domain/get_difficulty.py
@@ -0,0 +1,18 @@
+def get_difficulty():
+    """
+    function that allows the user to input the level of difficulty that wants for the game
+    validates the entry until the user inputs a valid option
+    :return: lives
+    """
+    while True:
+        difficulty = input("Insert which difficulty level you want: easy or hard?\n")
+        if difficulty == "easy":
+            lives = 10
+            break
+        elif difficulty == "hard":
+            lives = 5
+            break
+        else:
+            print(f"'{difficulty}' is not a valid level, the valid levels are 'easy' or 'hard'")
+
+    return lives
--- main_guessing_game.py
+++ main_guessing_game.py
@@ -1,18 +1,10 @@
 from domain import print_wellcome as pw
 from domain import pick_number as pn
+from domain import get_difficulty as gd
+
 
 pw.print_wellcome()
 number = pn.pick_number()
 
-while True:
-    difficulty = input("Insert which difficulty level you want: easy or hard?\n")
-    if difficulty == "easy":
-        lives = 10
-        break
-    elif difficulty == "hard":
-        lives = 5
-        break
-    else:
-        print(f"'{difficulty}' is not a valid level, the valid levels are 'easy' or 'hard'")
-
-print(f"You have {lives} lives!")
+user_lives = gd.get_difficulty()
+print(f"You have {user_lives} lives!")
