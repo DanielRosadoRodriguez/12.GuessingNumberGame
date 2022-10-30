ab77cdd3ed559ebde5938743b256af8be9ab408f
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-10-30 10:35:25 -0600

    feat: added option to pick difficulty and validate input

--- main_guessing_game.py
+++ main_guessing_game.py
@@ -1,5 +1,18 @@
 from domain import print_wellcome as pw
+from domain import pick_number as pn
 
 pw.print_wellcome()
+number = pn.pick_number()
 
+while True:
+    difficulty = input("Insert which difficulty level you want: easy or hard?\n")
+    if difficulty == "easy":
+        lives = 10
+        break
+    elif difficulty == "hard":
+        lives = 5
+        break
+    else:
+        print(f"'{difficulty}' is not a valid level, the valid levels are 'easy' or 'hard'")
 
+print(f"You have {lives} lives!")
