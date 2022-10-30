1f8119b32969caa9296dc457ce7607b1fe40887a
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-10-30 11:15:40 -0600

    feat: add main game functionality

--- main_guessing_game.py
+++ main_guessing_game.py
@@ -13,15 +13,17 @@ number = pn.pick_number()
 # lets the user pick a difficulty level and sets the user lives for the game
 user_lives = gd.get_difficulty()
 print(f"You have {user_lives} lives!")
-user_guess = gug.get_user_guess()
+
 
 while True:
+    user_guess = gug.get_user_guess()
     if user_guess == number:
         print("You Win!")
         break
     else:
         if user_lives > 0:
             user_lives -= 1
+            print(f"You have {user_lives} remaining")
         else:
             print("You Lose")
             break
