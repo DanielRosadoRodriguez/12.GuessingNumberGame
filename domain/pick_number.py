7ada033cc7abe6c226f45fd6a206e3ae06f67481
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-10-30 10:25:22 -0600

    feat [pick_number]: add pick number function

---
+++ domain/pick_number.py
@@ -0,0 +1,11 @@
+# import random library to use the function random.choice
+import random
+
+
+def pick_number():
+    """
+    Function that picks a random number between 1 - 100
+    :return: random_number
+    """
+    random_number = random.choice(range(1, 100))
+    return random_number
