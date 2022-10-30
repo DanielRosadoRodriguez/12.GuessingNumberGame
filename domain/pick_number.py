9ead9d9c83de9d06aca28da2f32254e54d4319ff
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-10-30 10:28:52 -0600

    fix [pick_number]: fix range previous: range(1, 100), new: range(1, 101), range now includes 100

--- domain/pick_number.py
+++ domain/pick_number.py
@@ -7,5 +7,5 @@ def pick_number():
     Function that picks a random number between 1 - 100
     :return: random_number
     """
-    random_number = random.choice(range(1, 100))
+    random_number = random.choice(range(1, 101))
     return random_number
