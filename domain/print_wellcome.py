95ab17c598f28ffea7ff5642b94b8c13fa7788fd
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-10-30 10:19:44 -0600

    feat [print_wellcome]: add print instructions for the game

--- domain/print_wellcome.py
+++ domain/print_wellcome.py
@@ -19,3 +19,7 @@ def print_wellcome():
     """
     print(game_logo)
     print("Wellcome to Number Guessing game")
+    print("The machine will pick a random number between 1 - 100 and you have to guess the number")
+    print("You can choose between 2 levels of difficulty: ")
+    print("     - Easy: 5 lives")
+    print("     - Hard: 10 lives")
--- main_guessing_game.py
+++ main_guessing_game.py
@@ -2,3 +2,4 @@ from domain import print_wellcome as pw
 
 pw.print_wellcome()
 
+
