fe77fc3a07566f84d8ea1f5315ba7b1435d59f46
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-10-30 11:32:30 -0600

    feat: add function that allows the user to keep playing until decides to exit the game

--- main_guessing_game.py
+++ main_guessing_game.py
@@ -2,29 +2,21 @@
 from domain import print_wellcome as pw
 from domain import pick_number as pn
 from domain import get_difficulty as gd
-from domain import get_user_guess as gug
-
+from domain import game_core as gc
 
 # print the wellcome to the user
 pw.print_wellcome()
-# generate the random number the user will guess
-number = pn.pick_number()
-
-# lets the user pick a difficulty level and sets the user lives for the game
-user_lives = gd.get_difficulty()
-print(f"You have {user_lives} lives!")
 
 
 while True:
-    user_guess = gug.get_user_guess()
-    if user_guess == number:
-        print("You Win!")
-        break
-    else:
-        if user_lives > 0:
-            user_lives -= 1
-            print(f"You have {user_lives} remaining")
-        else:
-            print("You Lose")
-            break
+    # generate the random number the user will guess
+    number = pn.pick_number()
 
+    # lets the user pick a difficulty level and sets the user lives for the game
+    user_lives = gd.get_difficulty()
+    print(f"You have {user_lives} lives!")
+    gc.game_core(lives=user_lives, machine_number=number)
+    play_again = input("Do you want to play again? y/n\n").lower()
+    if play_again == "n":
+        print("See you soon!")
+        break
