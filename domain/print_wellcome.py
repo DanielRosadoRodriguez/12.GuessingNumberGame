17cb552d3cf4071d9bda82b3cc998dcdd0335dd9
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-10-30 20:51:27 -0600

    fix: switched number of lives between easy and hard

--- domain/print_wellcome.py
+++ domain/print_wellcome.py
@@ -21,5 +21,5 @@ def print_wellcome():
     print("Wellcome to Number Guessing game")
     print("The machine will pick a random number between 1 - 100 and you have to guess the number")
     print("You can choose between 2 levels of difficulty: ")
-    print("     - Easy: 5 lives")
-    print("     - Hard: 10 lives")
+    print("     - Easy: 10 lives")
+    print("     - Hard: 5 lives")
